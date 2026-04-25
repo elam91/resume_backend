1c96a9175db89120d2986bc24369f36937c0db8f
Elam Buteil <elam91@gmail.com>
2026-04-25 14:29:39 +0300

    fix(main): pytz replacement

--- resume_backend/middleware.py
+++ resume_backend/middleware.py
@@ -1,6 +1,5 @@
 from os import getenv
-
-import pytz
+import zoneinfo  # <--- Use this instead of pytz
 from django.utils import timezone
 
 
@@ -9,7 +8,15 @@ class TimezoneMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # Get the TZ string from the environment
         environment_timezone = getenv('USER_TZ', 'Asia/Jerusalem')
-        activated_timezone = pytz.timezone(environment_timezone)
-        timezone.activate(activated_timezone)
+
+        try:
+            # Create a ZoneInfo object
+            activated_timezone = zoneinfo.ZoneInfo(environment_timezone)
+            timezone.activate(activated_timezone)
+        except zoneinfo.ZoneInfoNotFoundError:
+            # Fallback to UTC or a default if the env var is invalid
+            timezone.activate(zoneinfo.ZoneInfo('UTC'))
+
         return self.get_response(request)
